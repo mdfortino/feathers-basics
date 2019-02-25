a4eb75204e9654d28eb2f4e0c2e5830adf705ecb
“MARIA <MDFORTINO@GMAIL.COM>
2019-02-25 14:01:23 -0500

    delete models, services and hooks

--- src/services/index.js
+++
@@ -1,7 +0,0 @@
-const messages = require('./messages/messages.service.js');
-const users = require('./users/users.service.js');
-// eslint-disable-next-line no-unused-vars
-module.exports = function (app) {
-  app.configure(messages);
-  app.configure(users);
-};
