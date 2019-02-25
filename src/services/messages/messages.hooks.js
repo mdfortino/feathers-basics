5d5e616f936e821dece8b52c27b08f16753e6b05
“MARIA <MDFORTINO@GMAIL.COM>
2019-02-25 12:32:51 -0500

    generated populate-user hook

--- src/services/messages/messages.hooks.js
+++ src/services/messages/messages.hooks.js
@@ -2,6 +2,8 @@ const { authenticate } = require('@feathersjs/authentication').hooks;
 
 const processMessage = require('../../hooks/process-message');
 
+const populateUser = require('../../hooks/populate-user');
+
 module.exports = {
   before: {
     all: [ authenticate('jwt') ],
@@ -14,7 +16,7 @@ module.exports = {
   },
 
   after: {
-    all: [],
+    all: [populateUser()],
     find: [],
     get: [],
     create: [],
